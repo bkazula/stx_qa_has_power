6d1def15d7a8e7fdf07485817f4d845c52b8982b
Bartosz Kazuła <bartosz.kazula@gmail.com>
2018-05-24 20:11:03 +0200

    Fixed password length

--- auth/models.py
+++ auth/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(30))
+    password = db.Column(db.String(100))
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
