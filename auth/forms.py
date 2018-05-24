4155dfe40129ab86236d35f728f74c3ab40a5ded
Bartosz Kazuła <bartosz.kazula@gmail.com>
2018-05-24 21:14:23 +0200

    Password validation, improved validation for whole registration form

--- auth/forms.py
+++ auth/forms.py
@@ -1,17 +1,42 @@
+import re
+from sqlalchemy.orm.exc import NoResultFound
 from wtforms import Form, PasswordField, StringField, validators
-from wtforms.validators import DataRequired
+
+from .models import User
 
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=5, max=50)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
+    email = StringField('Email', [validators.DataRequired(), validators.Email(message='Niepoprawny adres email')])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
+        validators.Length(min=12, max=17, message='Hasło musi mieć co najmniej 12 znaków i nie więcej niz 17'),
+
     ])
-    confirm = PasswordField('Confirm Password')
+    confirm = PasswordField('Confirm Password', [
+        validators.EqualTo('confirm', message='Podane hasła są rózne')
+    ])
+
+    def validate_password(self, field):
+        value: str = field.data
+        rules = [
+            re.match('^([A-Z].*|.*[A-Z])$', value), # Wielka litera na początku lub końcu
+            re.match('.*[0-9].*', value), # Jedna cyfra
+            re.match('.*[a-z].*[a-z].*', value), # Dwie małe litery w dowolnym miejscu
+            re.match('.+[^\w\s].*', value), # Co najmniej jeden znak specjalny, nie na początku
+        ]
+        for rule in rules:
+            if not rule:
+                raise validators.ValidationError('Hasło musi zawierać minimum jedną duzą literę, 2 cyfry, znak specjalny i małe litery')
+
+    def validate_email(self, field):
+        try:
+            User.query.filter_by(email=field.data).one()
+            raise validators.ValidationError('Nie mozna zarejestrować podanego adresu email')
+        except NoResultFound:
+            pass
 
 
 class UserLoginForm(Form):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[validators.DataRequired()])
+    password = PasswordField('Password', validators=[validators.DataRequired()])
